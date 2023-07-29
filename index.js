622c955746a66d96f1c6f760808e1c49b9ffe908
AdityaShivhare7291 <adityashivhare7291@gmail.com>
2023-07-29 22:35:23 +0530

    database connectivuty

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const DatabaseUrl = 'mongodb+srv://adityashivhare7291:9981142668@cluster0.nf2t6aw.mongodb.net/sringar';
+const app = express();
 
-const DatabaseUrl = 'mongodb+srv://adityashivhare7291:9981142668@cluster0.nf2t6aw.mongodb.net/sringar'; // Replace 'my_database' with your database name
-const app=Express();
-
-const User = require('schema.js');
+app.use(express.json());
+// Import the User schema using the correct relative path
+const User = require('./schemas/schema'); // Replace './schema' with the actual relative path to your 'schema.js' file
 
 mongoose.connect(DatabaseUrl, {
   useNewUrlParser: true,
@@ -16,13 +17,17 @@ mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
 });
 
-app.post('http://localhost:3000/signup', (req, res) => {
+// You should use 'User' instead of 'Person'
+
+
+// Move the newPerson.save() block inside the app.post() route handler
+app.post('/signup', async(req, res) => {
   const infos = req.body;
 
   // Validate request data against the schema
   const newUser = new User(infos);
 
-  newUser.save((err, savedUser) => {
+  const result=await newUser.save((err, savedUser) => {
     if (err) {
       console.error('Error saving user:', err);
       return res.status(400).json({ error: 'Invalid data' });
@@ -32,9 +37,8 @@ app.post('http://localhost:3000/signup', (req, res) => {
   });
 });
 
+const port = 3000; // Set the port for your server
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-
-
