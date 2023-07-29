f67e74e3738b1dfbf16eadef1aec435fc73a1966
AdityaShivhare7291 <adityashivhare7291@gmail.com>
2023-07-29 22:57:38 +0530

    Better

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
+const cors = require("cors");
+
 
 const DatabaseUrl = 'mongodb+srv://adityashivhare7291:9981142668@cluster0.nf2t6aw.mongodb.net/sringar';
-const app = express();
 
-app.use(express.json());
-// Import the User schema using the correct relative path
-const User = require('./schemas/schema'); // Replace './schema' with the actual relative path to your 'schema.js' file
+
+
+const User = require('./schemas/schema.js');
 
 mongoose.connect(DatabaseUrl, {
   useNewUrlParser: true,
@@ -17,17 +19,28 @@ mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
 });
 
-// You should use 'User' instead of 'Person'
+
+app.use(express.json());
+
+// app.use(bodyParser.json());
+
+app.use(cors());
 
 
-// Move the newPerson.save() block inside the app.post() route handler
+
+// app.post('http://localhost:5000/signup', (req, res) => {
+
 app.post('/signup', async(req, res) => {
-  const infos = req.body;
+  // const infos = req.body;
 
   // Validate request data against the schema
-  const newUser = new User(infos);
+  // const newUser = new User(infos);
 
-  const result=await newUser.save((err, savedUser) => {
+  const { name, email, password, phoneNumber, gender } = req.body;
+
+  const newUser = new User({ name, email, password, phoneNumber, gender });
+
+  const result = await newUser.save((err, savedUser) => {
     if (err) {
       console.error('Error saving user:', err);
       return res.status(400).json({ error: 'Invalid data' });
@@ -37,8 +50,12 @@ app.post('/signup', async(req, res) => {
   });
 });
 
-const port = 3000; // Set the port for your server
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT =  5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
